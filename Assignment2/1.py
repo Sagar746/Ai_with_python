a55f8163e5b67530f803c9b0016b77c4f2cdfb04
Sagar746 <tiwarisagar389@gmail.com>
2025-01-27 14:14:07 +0200

    part one modified

--- Assignment2/1.py
+++ Assignment2/1.py
@@ -16,4 +16,13 @@ plt.figure(figsize=(8,6))
 plt.plot(x, y1, label=r'$y=2x+1$', color='blue', linestyle='-', linewidth='2')
 plt.plot(x, y2, label=r'$y=2x+2$', color='green', linestyle='--', linewidth='2')
 plt.plot(x, y3, label=r'$y=2x+3$', color='red', linestyle=':', linewidth='2')
+
+plt.title('Graph of y=2x+1, y=2x+2, y=2x+3', fontsize=14)
+plt.xlabel('x', fontsize=12)
+plt.ylabel('y', fontsize=12)
+
+plt.legend()
+
+plt.grid(True)
 plt.show()
+
